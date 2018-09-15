6bab50dede6c9a1a604878134dc65d800be35e85
Jason Chu <jjchu@uwaterloo.ca>
2018-09-15 17:50:33 -0400

    Upload photo activity is ready to connect to API.

--- app/src/main/java/com/lejrapp/lejr2/UploadPhotoActivity.java
+++ app/src/main/java/com/lejrapp/lejr2/UploadPhotoActivity.java
@@ -2,11 +2,26 @@ package com.lejrapp.lejr2;
 
 import android.content.Intent;
 import android.graphics.Bitmap;
+import android.net.Uri;
 import android.provider.MediaStore;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ProgressBar;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
+
+import java.util.UUID;
+
+import java.io.ByteArrayOutputStream;
+
+import static android.view.View.VISIBLE;
 
 public class UploadPhotoActivity extends AppCompatActivity {
 
@@ -14,19 +29,24 @@ public class UploadPhotoActivity extends AppCompatActivity {
 
     private Button cameraButton;
     private Button galleryButton;
+    private StorageReference storageReference;
+    private ProgressBar progressBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_upload_photo);
 
+        storageReference = FirebaseStorage.getInstance().getReference();
         cameraButton = findViewById(R.id.cameraButton);
         galleryButton = findViewById(R.id.galleryButton);
+        progressBar = findViewById(R.id.uploadingProgress);
 
         cameraButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 dispatchTakePictureIntent();
+                progressBar.setEnabled(true);
             }
         });
     }
@@ -37,8 +57,32 @@ public class UploadPhotoActivity extends AppCompatActivity {
             Bundle extras = data.getExtras();
             Bitmap imageBitmap = (Bitmap) extras.get("data");
 
+            ByteArrayOutputStream stream = new ByteArrayOutputStream();
+            imageBitmap.compress(Bitmap.CompressFormat.JPEG, 100, stream);
+            byte[] byteArray = stream.toByteArray();
+            imageBitmap.recycle();
+
             this.cameraButton.setEnabled(false);
             this.galleryButton.setEnabled(false);
+            this.progressBar.setVisibility(VISIBLE);
+
+            StorageReference imageRef = storageReference.child("images/" + UUID.randomUUID().toString());
+            imageRef.putBytes(byteArray)
+                .addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                    @Override
+                    public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                        // Get a URL to the uploaded content
+                        Uri downloadUrl = taskSnapshot.getDownloadUrl();
+                        progressBar.setVisibility(View.INVISIBLE);
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception exception) {
+                        System.out.println("Error: Couldn't upload to Firebase.");
+                        progressBar.setVisibility(View.INVISIBLE);
+                    }
+                });
         }
     }
 
