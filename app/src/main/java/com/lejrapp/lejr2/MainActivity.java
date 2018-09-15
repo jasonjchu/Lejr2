e2b241261285f068de4645e8e0b08ebb5706edab
Jason Chu <jjchu@uwaterloo.ca>
2018-09-15 16:32:52 -0400

    Initial commit.

---
+++ app/src/main/java/com/lejrapp/lejr2/MainActivity.java
@@ -0,0 +1,36 @@
+package com.lejrapp.lejr2;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    private EditText usernameText;
+    private EditText passwordText;
+    private Button loginButton;
+    private ProgressBar progressBar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        setContentView(R.layout.activity_main);
+        super.onCreate(savedInstanceState);
+
+        usernameText = findViewById(R.id.username);
+        passwordText = findViewById(R.id.password);
+        loginButton = findViewById(R.id.loginButton);
+        progressBar = findViewById(R.id.loginProgress);
+
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                loginButton.setEnabled(false);
+                progressBar.setVisibility(View.VISIBLE);
+                //TODO: Send request, change activity
+            }
+        });
+    }
+}
